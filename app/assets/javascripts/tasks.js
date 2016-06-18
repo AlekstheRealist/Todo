6d0459c01f227c3bb1581e7afca9986b6deb0eaa
Aleks Real <aleksanderreal@gmail.com>
2016-06-18 00:28:06 +0000

    Added css and design and added some finishing touches

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,7 +4,9 @@
     // <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
@@ -21,16 +23,20 @@
     function toggleTask(e) {
       var itemId = $(e.target).data("id");
 
-
       var doneValue = Boolean($(e.target).is(':checked'));
-
 
       $.post("/tasks/" + itemId, {
         _method: "PUT",
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
+
+      } );
     }
 
     $.get("/tasks").success( function( data ) {
@@ -45,6 +51,7 @@
       $('.toggle').change(toggleTask);
 
     });
+
 
     $('#new-form').submit(function(event) {
       event.preventDefault();
@@ -61,4 +68,5 @@
         $('.toggle').click(toggleTask);
       });
     });
+
   });
